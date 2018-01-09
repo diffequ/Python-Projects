4c711e3d6bed7e62211f4b126220746d18aa0e7e
Burak <b.fk@unb.ca>
2018-01-09 16:46:35 -0400

    Add files via upload

---
+++ P5-Temp.py
@@ -0,0 +1,14 @@
+def main():
+    print("\n\t    Thermometer project with PICxxxx and Python")
+    print("\t         UNB @ Fredericton - January 2017\n")
+
+    print("Pick one of the following options:")
+    print("1) Measure temperature")
+    print("2) Turn on cooler fan")
+    print("3) Write temperature data on file")
+    print("4) Set threshold value for temperature\n")
+
+    choice = input("Enter your choice:")
+    print(choice)
+
+main()
