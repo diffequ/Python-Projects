fe18d7b85150adf348e7e056f1fb1c2b93d3da0c
Burak <b.fk@unb.ca>
2018-01-29 22:27:09 -0400

    Update P5-LM35PIC.py

--- P5-LM35PIC.py
+++ P5-LM35PIC.py
@@ -1,5 +1,6 @@
-# LM35 Temperature sensor with PIC16F877 and Python Interface
-# Burak Koryan | e-mail : burak@koryan.ca | web: http://koryan.ca
+# LM35 Temperature sensor with PIC16F877 and Python Interface         #
+# Burak Koryan | e-mail : burak@koryan.ca | web: http://koryan.ca     #  
+#######################################################################
 
 import time
 import serial
